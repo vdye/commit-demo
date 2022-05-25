692f477dc29e05f39b8140f4254c61adbe15924a
Victoria Dye <vdye@github.com>
2022-05-25 16:29:14 -0700

    Finish script
    Now reads image from input

--- image-modifier.py
+++ image-modifier.py
@@ -1,7 +1,27 @@
+import os
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-img = mpimg.imread('images/test-landscape.jpg')
 
-imgplot = plt.imshow(img)
-plt.show()
+def main():
+    # Parse args
+    parser = argparse.ArgumentParser(description='Manipulate and display images.')
+    parser.add_argument('image', type=str,
+                        help='the image to process')
+    args = parser.parse_args()
+
+    # Verify file validity
+    if not os.path.isfile(args.image):
+        parser.error(f'Specified file "{args.image}" does not exist!')
+
+    # Read the image
+    img = mpimg.imread(args.image)
+
+    # Display the image
+    plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+    plt.axis('off')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
